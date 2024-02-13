cc63225c1474714c1fcd6ea385c4ba94ce3d6fbf
qdanix43 <159129151+qdanix43@users.noreply.github.com>
2024-02-13 12:41:24 +0100

    actualicacion calculadora

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.SeekBar
 import android.widget.TextView
+import kotlin.math.pow
+
 class MainActivity : AppCompatActivity() {
     private var peso: Int = 50
+    private var altura: Int = 150
+    private lateinit var seekBar: SeekBar
     private lateinit var textViewPeso: TextView
+    private lateinit var textViewAltura: TextView
+    private lateinit var miBotonCualquiera: Button
+    private lateinit var textViewResultado: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,19 +23,60 @@ class MainActivity : AppCompatActivity() {
 
         textViewPeso = findViewById(R.id.textViewPeso)
         actualizarTextoPeso()
+        textViewAltura = findViewById(R.id.textView1)
+        seekBar = findViewById(R.id.seekBar)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                altura = progress
+                actualizarTextoAltura()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        miBotonCualquiera = findViewById(R.id.buttonCalcular)
+        textViewResultado = findViewById(R.id.textViewResultado)
+
+        miBotonCualquiera.setOnClickListener {textViewResultado
+            val resultado = calcularIMC()
+            textViewResultado.text = "Tu IMC es de $resultado"
+        }
     }
 
-    fun subirPeso(view: android.view.View) {
+    fun subirPeso(view: View) {
         peso++
         actualizarTextoPeso()
     }
 
-    fun bajarPeso(view: android.view.View) {
+    fun bajarPeso(view: View) {
         peso--
         actualizarTextoPeso()
     }
 
+    fun subirAltura(view: View) {
+        altura++
+        actualizarTextoAltura()
+        seekBar.progress = altura
+    }
+
+    fun bajarAltura(view: View) {
+        altura--
+        actualizarTextoAltura()
+        seekBar.progress = altura
+    }
+
     private fun actualizarTextoPeso() {
-        textViewPeso.text = "Peso: $peso"
+        textViewPeso.text = "Peso: $peso kg"
+    }
+
+    private fun actualizarTextoAltura() {
+        textViewAltura.text = "Altura: ${(altura / 100f)} m"
+    }
+
+    private fun calcularIMC(): String {
+        val resultado = peso / (altura / 100f).pow(2)
+        return resultado.toString()
     }
 }
+
